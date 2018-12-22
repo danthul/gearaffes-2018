32f9e4f548a761f51213c279394652b2088707c4
ftcstudent <ftcgearaffes@gmail.com>
2018-12-22 14:10:19 -0600

    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDepot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDepot.java
@@ -38,7 +38,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-
+// Hello
 /**
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
